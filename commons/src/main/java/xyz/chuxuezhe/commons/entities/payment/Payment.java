ea0309d595310e3e23a3f938042c1706bfc1de2f
EugeneGe <geluyao@smartever.cn>
2020-12-22 12:00:03 +0800

    Commons 模块测试正常

---
+++ commons/src/main/java/xyz/chuxuezhe/commons/entities/payment/Payment.java
@@ -0,0 +1,20 @@
+package xyz.chuxuezhe.commons.entities.payment;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author Yang Hao
+ * @description
+ * @date 2020-09-14 15:06
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+    private Long id;
+    private String serial;
+}
