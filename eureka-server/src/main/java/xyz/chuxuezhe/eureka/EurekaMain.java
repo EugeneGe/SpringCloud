330bccdb39f268149810eccce4f70f868c156fde
EugeneGe <geluyao@smartever.cn>
2020-12-22 14:39:11 +0800

    Eureka 服务器注册

---
+++ eureka-server/src/main/java/xyz/chuxuezhe/eureka/EurekaMain.java
@@ -0,0 +1,19 @@
+package xyz.chuxuezhe.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka服务器
+ *
+ * @author EugeneGe
+ * @date 2020-12-22 14:32
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain.class, args);
+    }
+}
