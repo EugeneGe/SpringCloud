3604fdcb6c76209230eb6b93cd891b4acf462eb1
EugeneGe <geluyao@smartever.cn>
2020-12-22 14:49:47 +0800

    eureka 注册

--- order-1.0/src/main/java/xyz/chuxuezhe/order/OrderMain.java
+++ order-1.0/src/main/java/xyz/chuxuezhe/order/OrderMain.java
@@ -2,12 +2,14 @@ package xyz.chuxuezhe.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author EugeneGe
  * @date 2020-12-22 10:41
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain.class, args);
--- payment-1.0/src/main/java/xyz/chuxuezhe/payment/PaymentMain.java
+++ payment-1.0/src/main/java/xyz/chuxuezhe/payment/PaymentMain.java
@@ -2,12 +2,14 @@ package xyz.chuxuezhe.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author EugeneGe
  * @date 2020-12-21 17:22
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain.class, args);
