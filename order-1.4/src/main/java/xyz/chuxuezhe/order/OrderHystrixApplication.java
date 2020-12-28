378bd94a7e9fe882480ffd3d0868c9a0a7ad9d10
EugeneGe <geluyao@smartever.cn>
2020-12-28 16:59:44 +0800

    Hystrix 服务降级熔断测试

--- order-1.4/src/main/java/xyz/chuxuezhe/order/OrderHystrixApplication.java
+++ order-1.4/src/main/java/xyz/chuxuezhe/order/OrderHystrixApplication.java
@@ -2,20 +2,17 @@ package xyz.chuxuezhe.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
- * EnableDiscoveryClient 该注解用于向使用consul或者zookeeper作为注册中心时注册服务
- *
  * @author EugeneGe
  */
 @SpringBootApplication
-//@EnableDiscoveryClient
 @EnableFeignClients
-public class OrderFeign80Application {
+@EnableHystrix
+public class OrderHystrixApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrderFeign80Application.class, args);
+        SpringApplication.run(OrderHystrixApplication.class);
     }
-
 }
