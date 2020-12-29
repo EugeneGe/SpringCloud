2eff3bfd37bc191332afba1b2ccbfd81f8f09f67
EugeneGe <geluyao@smartever.cn>
2020-12-29 16:09:50 +0800

    gateway 路由转发

--- gateway/src/main/java/xyz/chuxuezhe/gateway/GatewayApplication.java
+++ gateway/src/main/java/xyz/chuxuezhe/gateway/GatewayApplication.java
@@ -2,11 +2,13 @@ package xyz.chuxuezhe.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author EugeneGe
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
